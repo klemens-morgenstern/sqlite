6671370255425f7d0f406e0dc933980891cc5410
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2024-03-28 09:21:53 +0800

    added transaction guards.

---
+++ include/boost/sqlite/transaction.hpp
@@ -0,0 +1,171 @@
+//
+// Copyright (c) 2024 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_SQLITE_TRANSACTION_HPP
+#define BOOST_SQLITE_TRANSACTION_HPP
+
+#include <boost/sqlite/connection.hpp>
+
+BOOST_SQLITE_BEGIN_NAMESPACE
+
+/// A simple transaction guard implementing RAAI for transactions
+struct transaction
+{
+  /// The mode of the transaction
+  enum behaviour {deferred, immediate, exclusive};
+  /// A tag to use, to adopt an already initiated transaction.
+  constexpr static struct adopt_transaction_t {} adopt_transaction{};
+
+
+  /// Create transaction guard on an existing transaction
+  transaction(connection & conn, adopt_transaction_t) : conn_(conn), completed_(false)
+  {
+  }
+
+
+  /// Create transaction guard and initiate a transaction
+  transaction(connection & conn) : conn_(conn)
+  {
+    conn.execute("BEGIN");
+    completed_ = false;
+  }
+
+  /// Create transaction guard and initiate a transaction with the defined behaviour
+  transaction(connection & conn, behaviour b) : conn_(conn)
+  {
+    switch (b)
+    {
+      case deferred:  conn.execute("BEGIN DEFERRED"); break;
+      case immediate: conn.execute("BEGIN IMMEDIATE"); break;
+      case exclusive: conn.execute("BEGIN EXCLUSIVE"); break;
+    }
+    completed_ = false;
+  }
+
+  /// rollback the transaction if not committed.
+  ~transaction() noexcept(false)
+  {
+    if (!completed_)
+      conn_.execute("ROLLBACK");
+  }
+
+  ///@{
+  /// Commit the transaction.
+  void commit()
+  {
+    conn_.execute("COMMIT");
+    completed_ = true;
+  }
+
+  void commit(system::error_code & ec, error_info & ei)
+  {
+    conn_.execute("COMMIT", ec, ei);
+    completed_ = true;
+  }
+  ///@}
+
+  ///@{
+  /// Rollback the transaction explicitly.
+  void rollback()
+  {
+    conn_.execute("ROLLBACK");
+    completed_ = true;
+  }
+
+  void rollback(system::error_code & ec, error_info & ei)
+  {
+    conn_.execute("ROLLBACK", ec, ei);
+    completed_ = true;
+  }
+  ///@}
+
+ private:
+  connection & conn_;
+  bool completed_ = true;
+};
+
+/// A simple transaction guard implementing RAAI for transactions
+struct savepoint
+{
+  /// A tag to use, to adopt an already initiated transaction.
+  constexpr static transaction::adopt_transaction_t adopt_transaction{};
+
+  /// Create savepoint guard on an existing savepoint
+  savepoint(connection & conn, std::string name, transaction::adopt_transaction_t)
+      : conn_(conn), name_(std::move(name))
+  {
+  }
+
+  /// Create transaction guard and initiate it
+  savepoint(connection & conn, std::string name) : conn_(conn), name_(std::move(name))
+  {
+    conn.execute("SAVEPOINT " + name_);
+    completed_ = false;
+  }
+
+
+  /// rollback to the savepoint if not committed.
+  ~savepoint() noexcept(false)
+  {
+    if (!completed_)
+      conn_.execute("ROLLBACK TO " + name_);
+  }
+
+  ///@{
+  /// Commit/Release the transaction.
+  void commit()
+  {
+    conn_.execute("RELEASE " + name_);
+    completed_ = true;
+  }
+
+  void commit(system::error_code & ec, error_info & ei)
+  {
+    conn_.execute("RELEASE " + name_, ec, ei);
+    completed_ = true;
+  }
+
+  void release()
+  {
+    conn_.execute("RELEASE " + name_);
+    completed_ = true;
+  }
+
+  void release(system::error_code & ec, error_info & ei)
+  {
+    conn_.execute("RELEASE " + name_, ec, ei);
+    completed_ = true;
+  }
+  ///@}
+
+  ///@{
+  /// Rollback the transaction explicitly.
+  void rollback()
+  {
+    conn_.execute("ROLLBACK TO" + name_);
+    completed_ = true;
+  }
+
+  void rollback(system::error_code & ec, error_info & ei)
+  {
+    conn_.execute("ROLLBACK TO " + name_, ec, ei);
+    completed_ = true;
+  }
+  ///@}
+
+  /// The name of the savepoint.
+  const std::string & name() const {return name_;}
+ private:
+  connection & conn_;
+  std::string name_;
+  bool completed_ = true;
+};
+
+
+BOOST_SQLITE_END_NAMESPACE
+
+#endif //BOOST_SQLITE_TRANSACTION_HPP
---
+++ test/transaction.cpp
@@ -0,0 +1,84 @@
+//
+// Copyright (c) 2024 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/sqlite/transaction.hpp>
+
+#include "test.hpp"
+
+using namespace boost;
+
+BOOST_AUTO_TEST_CASE(transaction)
+{
+  sqlite::connection conn{":memory:"};
+  conn.execute("create table test(nr integer);");
+
+  auto check_size = [&]{
+    std::size_t n = 0ull;
+    for (auto l : conn.query("select * from test"))
+      n++;
+    return n;
+  };
+
+  {
+    sqlite::transaction t{conn};
+    BOOST_CHECK_THROW(sqlite::transaction{conn}, system::system_error);
+
+    conn.execute("insert into test values(1), (2)");
+    BOOST_CHECK(check_size() == 2);
+
+    {
+      sqlite::savepoint sq{conn, "s1"};
+      conn.execute("insert into test values(3)");
+      BOOST_CHECK(check_size() == 3);
+    }
+
+    BOOST_CHECK(check_size() == 2);
+
+    {
+      sqlite::savepoint sq{conn, "s1"};
+      conn.execute("insert into test values(4)");
+      BOOST_CHECK(check_size() == 3);
+      sq.commit();
+    }
+
+    BOOST_CHECK(check_size() == 3);
+
+    {
+      sqlite::savepoint sq{conn, "s1"};
+      conn.execute("insert into test values(5)");
+      BOOST_CHECK(check_size() == 4);
+      {
+        sqlite::savepoint sq2{conn, "s2"};
+        conn.execute("insert into test values (6), (7)");
+        BOOST_CHECK(check_size() == 6);
+        sq2.commit();
+      }
+      BOOST_CHECK_EQUAL(check_size(), 6);
+    }
+    BOOST_CHECK_EQUAL(check_size(), 3);
+
+  }
+
+  BOOST_CHECK(conn.query("select * from test").done());
+
+  {
+    system::error_code ec;
+    sqlite::error_info ei;
+    conn.execute("BEGIN", ec, ei);
+    BOOST_CHECK(!ec);
+    conn.execute("BEGIN", ec, ei);
+    BOOST_CHECK(ec);
+
+    BOOST_CHECK_THROW(sqlite::transaction{conn}, system::system_error);
+    sqlite::transaction t{conn, sqlite::transaction::adopt_transaction};
+
+    conn.execute("insert into test values (42), (3);");
+    t.commit();
+  }
+
+  BOOST_CHECK_EQUAL(check_size(), 2);
+}
